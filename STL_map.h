f18a42c7622e5247000ebaa648b85413a336637d
ItisMMQ <39751961+ItisMMQ@users.noreply.github.com>
2019-03-27 16:40:01 +0800

    Add files via upload

---
+++ STL_map.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Person {
+public:
+	friend ostream& operator<< (ostream& out, const Person& p) {
+		out << "(" << p.name << ", " << p.age << ") ";
+		return out;
+	}
+	Person(const char *n = "", int a = 0) {
+		name =_strdup(n);
+		age = a;
+	}
+	bool operator<(const Person& p) const {
+		return strcmp(name, p.name) < 0;
+	}
+	bool operator==(const Person& p) const {
+		return strcmp(name, p.name) == 0 && age == p.age;
+	}
+private:
+	char *name;
+	int age;
+};
+
+int map_test() {
+	pair<Person,const char*> p[] ={
+		pair<Person,const char*>(Person("Gregg",25),"Pittsburgh"),
+		pair<Person,const char*>(Person("Ann",30),"Boston"),
+		pair<Person,const char*>(Person("Bill",20),"Belmont")
+	};
+	map<Person,const  char*> cities;
+	cities.insert(p[0]);
+	cities.insert(p[1]);
+	cities.insert(p[2]);
+	cities[Person("Kay", 40)] = "New York";
+	cities["Jenny"] = "Newark";
+	cities.insert(map<Person,const char*>::value_type(Person("Kay", 40), "Detroit"));
+	cities.insert(pair<Person, const char*>(Person("Kay", 40), "Austin"));
+	map<Person, const char*>::iterator i;
+	for (i = cities.begin(); i != cities.end(); i++)
+		cout << (*i).first << ' ' << (*i).second << endl;
+	// output:
+	//   (Ann,30) Boston 
+	//   (Bill,20) Belmont 
+	//   (Gregg,25) Pittsburgh 
+	//   (Jenny,0) Newark 
+	//   (Kay, 40) New York
+	cities[p[1].first] = "Chicago";
+	for (i = cities.begin(); i != cities.end(); i++)
+		cout << (*i).first << ' ' << (*i).second << endl;
+	// output:
+	//   (Ann,30) Chicago 
+	//   (Bill,20) Belmont 
+	//   (Gregg,25) Pittsburgh 
+	//   (Jenny,0) Newark 
+	//   (Kay, 40) New York
+
+	multimap<Person, const char*> mCities;
+	mCities.insert(p[0]);
+	mCities.insert(p[1]);
+	mCities.insert(p[2]);
+	mCities.insert(pair<Person, const char*>(Person("Kay", 40), "Austin"));
+	mCities.insert(pair<Person, const char*>(Person("Kay", 40), "Austin"));
+	mCities.insert(pair<Person, const char*>(Person("Kay", 40), "Detroit"));
+	std::multimap<Person, const char*>::iterator mi;
+	for (mi = mCities.begin(); mi != mCities.end(); ++mi)
+		cout << (*mi).first << ' ' << (*mi).second << endl;
+	//Output:
+	//   (Ann,30) Boston 
+	//   (Bill,20) Belmont 
+	//   (Gregg,25) Pittsburgh 
+	//   (Kay, 40) Austin
+	//   (Kay, 40) Austin
+	//   (Kay, 40) Detroit
+	(*(mCities.find(Person("Kay", 40)))).second = "New York";
+	for (mi = mCities.begin(); mi != mCities.end(); mi++)
+		cout << (*mi).first << ' ' << (*mi).second << endl;
+	//Output:
+	//   (Ann,30) Boston 
+	//   (Bill,20) Belmont 
+	//   (Gregg,25) Pittsburgh 
+	//   (Kay, 40) New York
+	//   (Kay, 40) Austin
+	//   (Kay, 40) Detroit
+	return 0;
+}
